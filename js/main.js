9c58c6db070154f6d6c63b136af1569533046af5
David Stinson <david.stinson.5@gmail.com>
2020-01-13 15:20:16 -0600

    functionalized game logic

--- js/main.js
+++ js/main.js
@@ -1,24 +1,21 @@
 
 let cards = ["queen", "queen", "king", "king"]
 let cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[2];
 
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo)
-
-if ( cardsInPlay.length == 2 ) {
-    if ( cardsInPlay[0] === cardsInPlay[1] ) {
-      alert("Oh wow! You found a match!")
-    } else {
-      alert("Boo no match :( Try again!")
-    }
+function checkForMatch() {
+  if ( cardsInPlay[0] === cardsInPlay[1] ) {
+    console.log("Oh wow! You found a match!")
+  } else {
+    console.log("Boo no match :( Try again!")
+  }
 }
 
-
-
-
-
-console.log("User flipped: " + cardOne);
-console.log("User flipped: " + cardTwo);
+function flipCard(cardId) {
+  cardsInPlay.push(cards[cardId]);
+  console.log("You flipped the " + cards[cardId] + " card!");
+  if (cardsInPlay.length == 2) {
+    checkForMatch();
+  }
+}
+flipCard(0);
+flipCard(2);
