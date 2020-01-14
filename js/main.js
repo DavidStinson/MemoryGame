b3355bbcc0e39b5d1fae1a65a735000c467bf490
David Stinson <david.stinson.5@gmail.com>
2020-01-14 12:31:51 -0600

    changed card data type from array to object

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 
-let cards = ["queen", "queen", "king", "king"]
+let cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+]
 let cardsInPlay = [];
 
 function checkForMatch() {
@@ -11,11 +32,12 @@ function checkForMatch() {
 }
 
 function flipCard(cardId) {
-  cardsInPlay.push(cards[cardId]);
-  console.log("You flipped the " + cards[cardId] + " card!");
+  cardsInPlay.push(cards[cardId].rank);
+  console.log("You flipped the " + cards[cardId].rank + " of " + cards[cardId].suit);
+  console.log("This is an image of the card: " + cards[cardId].cardImage);
   if (cardsInPlay.length == 2) {
     checkForMatch();
   }
 }
 flipCard(0);
-flipCard(2);
+flipCard(1);
