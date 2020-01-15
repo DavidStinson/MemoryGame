ead6f5f934226aa45b8b8d94ce0f98a7080e5b5c
David Stinson <david.stinson.5@gmail.com>
2020-01-15 12:44:00 -0600

    added greater interactivity to the game

--- js/main.js
+++ js/main.js
@@ -23,21 +23,50 @@ let cards = [
 ]
 let cardsInPlay = [];
 
+function createBoard() {
+  for (let i = 0; i < cards.length; i++){
+    let cardElement = document.createElement("img");
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("id", i);
+    cardElement.addEventListener("click", flipCard);
+    document.getElementById("gameBoard").appendChild(cardElement);
+  }
+}
+
+function resetButtonListener() {
+  let resetButton = document.getElementById("resetButton");
+  resetButton.addEventListener("click", resetGame);
+}
+
+function resetBoard() {
+  for (let i = 0; i < cards.length; i++){
+    let cardElement = document.getElementById(i);
+    cardElement.setAttribute("src", "images/back.png");
+  }
+}
+
+function resetGame() {
+  resetBoard();
+  cardsInPlay = [];
+}
+
 function checkForMatch() {
   if ( cardsInPlay[0] === cardsInPlay[1] ) {
     console.log("Oh wow! You found a match!")
   } else {
     console.log("Boo no match :( Try again!")
   }
+  window.setTimeout(resetGame, 1000)
 }
 
-function flipCard(cardId) {
+function flipCard() {
+  let cardId = this.getAttribute("id");
   cardsInPlay.push(cards[cardId].rank);
   console.log("You flipped the " + cards[cardId].rank + " of " + cards[cardId].suit);
-  console.log("This is an image of the card: " + cards[cardId].cardImage);
+  this.setAttribute("src", cards[cardId].cardImage)
   if (cardsInPlay.length == 2) {
     checkForMatch();
   }
 }
-flipCard(0);
-flipCard(1);
+createBoard();
+resetButtonListener();
