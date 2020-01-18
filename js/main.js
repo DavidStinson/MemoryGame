be50e3e7e1c860f37e364bd1ae70b2a89ab77c2d
David Stinson <david.stinson.5@gmail.com>
2020-01-17 20:19:25 -0600

    Added card randomization, and cheating prevention.

--- js/main.js
+++ js/main.js
@@ -22,17 +22,34 @@ let cards = [
   }
 ]
 let cardsInPlay = [];
+let cardsRandom = [];
+let cardInPlayId = -1;
 let playerWins = 0;
 let playerLosses = 0;
 let playerWinsMessage = "";
 let playerLossesMessage = "";
 
+
+function cardRandomizer() {
+  let cardsToShuffle = [];
+  for (let i = 0; i < cards.length; i++) {
+    cardsToShuffle[i] = cards[i];
+  }
+  let i = 0;
+  while (i < cards.length) {
+    let randomIndex = Math.floor(Math.random()*cardsToShuffle.length)
+    cardsRandom[i] = cardsToShuffle[randomIndex];
+    cardsToShuffle.splice(randomIndex, 1);
+    i++;
+  }
+}
+
 function createBoard() {
-  for (let i = 0; i < cards.length; i++){
+  for (let i = 0; i < cardsRandom.length; i++){
     let cardElement = document.createElement("img");
     cardElement.setAttribute("src", "images/back.png");
     cardElement.setAttribute("id", i);
-    cardElement.addEventListener("click", flipCard);
+    cardElement.addEventListener("click", processGuess);
     document.getElementById("gameBoard").appendChild(cardElement);
   }
 }
@@ -50,32 +67,48 @@ function resetBoard() {
 }
 
 function resetGame() {
+  cardRandomizer();
   resetBoard();
   cardsInPlay = [];
+  cardInPlayId = -1;
+}
+
+function pauseBoard() {
+  for (let i = 0; i < cardsRandom.length; i++){
+    let cardElement = document.getElementById(i);
+    cardElement.removeEventListener("click", processGuess);
+  }
+}
+
+function resumeBoard() {
+  for (let i = 0; i < cardsRandom.length; i++){
+    let cardElement = document.getElementById(i);
+    cardElement.addEventListener("click", processGuess);
+  }
+  resetGame();
 }
 
 function playerWon(playerWon) {
   playerWon === true ? playerWins++ : playerLosses++;
   switch (playerWins) {
     case 0:
-      playerWinsMessage = "You have not won yet"
+      playerWinsMessage = "You have not won yet";
       break;
     case 1:
-      playerWinsMessage = "You have won 1 time"
+      playerWinsMessage = "You have won 1 time";
       break;
     default:
-      playerWinsMessage = "You have won " + playerWins + " times"
+      playerWinsMessage = "You have won " + playerWins + " times";
   }
   switch (playerLosses) {
     case 0:
-      playerLossesMessage = "have not lost yet."
+      playerLossesMessage = "have not lost yet.";
       break;
     case 1:
-      playerLossesMessage = "have lost once."
+      playerLossesMessage = "have lost once.";
       break;
     default:
-      playerLossesMessage = "have lost " + playerLosses + " times."
-
+      playerLossesMessage = "have lost " + playerLosses + " times.";
   }
   console.log(playerWinsMessage + " and " + playerLossesMessage);
 }
@@ -88,17 +121,25 @@ function checkForMatch() {
     console.log("Boo no match :( Try again!")
     playerWon(false);
   }
-  window.setTimeout(resetGame, 1000)
+  window.setTimeout(resumeBoard, 1000)
 }
 
-function flipCard() {
+function processGuess() {
   let cardId = this.getAttribute("id");
-  cardsInPlay.push(cards[cardId].rank);
-  console.log("You flipped the " + cards[cardId].rank + " of " + cards[cardId].suit);
-  this.setAttribute("src", cards[cardId].cardImage)
-  if (cardsInPlay.length == 2) {
-    checkForMatch();
+  if (cardInPlayId == cardId) {
+    console.log("You're not trying to cheat are you?!")
+  } else {
+    cardInPlayId = cardId;
+    cardsInPlay.push(cardsRandom[cardId].rank);
+    console.log("You flipped the " + cardsRandom[cardId].rank +
+      " of " + cardsRandom[cardId].suit);
+    this.setAttribute("src", cardsRandom[cardId].cardImage)
+    if (cardsInPlay.length == 2) {
+      pauseBoard();
+      checkForMatch();
+    }
   }
 }
+cardRandomizer();
 createBoard();
 resetButtonListener();
